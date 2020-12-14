5fd33cccbf292e259f28517bd6878d02bf588dc9
Brian Alejandro <brianalejandro1417@gmail.com>
2020-12-13 19:56:36 -0500

    4.variables y strings

--- clase1.js
+++ clase1.js
@@ -1,4 +1,11 @@
-let nombre = "Brisimb", apellido = "Gonzalez";
-console.log('Hola ' + nombre + ' ' + apellido);
-let edad = 16;
-console.log("Tengo " + edad + "años");
+let nombre = "Brian", apellido = "Gonzalez"
+let nombreEnMayusculas = nombre.toUpperCase()
+let apellidoEnMinusculas = apellido.toLowerCase();
+let primeraLetraDelNombre = nombre.charAt(0);
+let cantidadDeLetrasDelNombre = nombre.length;
+let nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+let str = nombre.substr(1,3);
+console.log(nombreEnMayusculas, apellidoEnMinusculas,primeraLetraDelNombre,cantidadDeLetrasDelNombre, nombreCompleto, str);
+//deafio: mostrar la ultima letra del nombre
+let ultimaLetraDelNombre = nombre.charAt(nombre.length-1);
+console.log(`La ultima letra del nombre es: ${ultimaLetraDelNombre}`);
